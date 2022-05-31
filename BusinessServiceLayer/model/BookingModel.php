e061af054bb6c5736061bcecff38eeae4beb27af
Minwei1999 <aaroncmw5621@gmail.com>
2022-06-01 00:30:51 +0800

    Update & Delete V2

--- BusinessServiceLayer/model/BookingModel.php
+++ BusinessServiceLayer/model/BookingModel.php
@@ -26,5 +26,27 @@ class bookingmodel {
         //To retrieve all good information from good table and send them to goodController class.
         $sql = "select * from booking";
         return DB::run($sql);
+		
+		function deleteBooking($BookingID){
+		$sql = "DELETE FROM booking WHERE BookingID =$delete";
+		
+		DB::run($sql);
+		}
+		
+		function updatebooking($BookingID,$ItemID,$id,$ItemQuantity,$DateBooking,$BookingTime,$PickUpDate) {
+	
+		$sql = "BEGIN;UPDATE booking SET(BookingID=':BookingID',ItemID=':ItemID',id=':id',ItemQuantity=':ItemQuantity',DateBooking=':DateBooking',BookingTime=':BookingTime',PickUpDate=':PickUpDate');COMMIT";
+		
+		$args = [
+		':BookingID'=>$BookingID,
+		':ItemID'=>$ItemID,
+		':id' =>$id,
+		':ItemQuantity'=>$ItemQuantity,
+		':DateBooking'=>$DateBooking,
+		':BookingTime'=>$BookingTime,
+		':PickUpDate'=>$PickUpDate
+		];
+		DB::run($sql, $args);
+		}
     }
 }
